43df7cca3c4540b9593987eeaaacf8cf11f6a13d
jakemeir <jakemeirjacobs@gmail.com>
2025-10-29 14:29:57 +0200

    add the info service and components

--- src/app/components/info/info.ts
+++ src/app/components/info/info.ts
@@ -1,13 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InfoService } from '../../services/info.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-info',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './info.html',
   styleUrl: './info.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Info {
+export class Info implements OnInit {
+  private infoService = inject(InfoService);
+  private destroyRef = inject(DestroyRef);
+  public networkElements = this.infoService.networkElements$;
 
+  ngOnInit() {
+    this.infoService.getNetworkElements().pipe(takeUntilDestroyed(this.destroyRef)).subscribe();
+  }
 }
--- src/app/services/info.service.ts
+++ src/app/services/info.service.ts
@@ -1,6 +1,93 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { catchError, tap, throwError } from 'rxjs';
+import { ErrorService } from './error.service';
+
+export interface NetworkElement {
+  id: string;
+  type: string;
+  ip_address: string;
+  mac_address: string;
+  hostname: string;
+  manufacturer: string;
+  model: string;
+  status: string;
+  location: string;
+  last_seen: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
-export class InfoService {}
+export class InfoService {
+  private httpClient = inject(HttpClient);
+  private errorService = inject(ErrorService);
+  private networkElements = signal<NetworkElement[]>([]);
+
+  public readonly networkElements$ = this.networkElements.asReadonly();
+
+  private httpRequest<T>(method: string, url: string, errorMessage: string, body?: Partial<T>) {
+    return this.httpClient
+      .request<T>(method, url, {
+        body,
+      })
+      .pipe(
+        catchError((error) => {
+          this.errorService.showError(errorMessage);
+          return throwError(() => new Error(errorMessage));
+        })
+      );
+  }
+
+  public getNetworkElements() {
+    return this.httpRequest<NetworkElement[]>(
+      'GET',
+      'http://localhost:3000/elements',
+      'Failed to fetch network elements.'
+    ).pipe(
+      tap((elements) => {
+        this.networkElements.set(elements);
+      })
+    );
+  }
+
+  public addNetworkElement(element: Partial<NetworkElement>) {
+    return this.httpRequest<NetworkElement>(
+      'POST',
+      'http://localhost:3000/elements',
+      'Failed to add network element.',
+      element
+    ).pipe(
+      tap((newElement) => {
+        this.networkElements.update((elements) => [...elements, newElement]);
+      })
+    );
+  }
+
+  public updateNetworkElement(id: string, element: Partial<NetworkElement>) {
+    return this.httpRequest<NetworkElement>(
+      'PUT',
+      `http://localhost:3000/elements${id}`,
+      'Failed to update network element.',
+      element
+    ).pipe(
+      tap((updatedElement) => {
+        this.networkElements.update((elements) =>
+          elements.map((el) => (el.id === id ? updatedElement : el))
+        );
+      })
+    );
+  }
+
+  public deleteNetworkElement(id: string) {
+    return this.httpRequest(
+      'DELETE',
+      `http://localhost:3000/elements${id}`,
+      'Failed to delete network element.'
+    ).pipe(
+      tap(() => {
+        this.networkElements.update((elements) => elements.filter((el) => el.id !== id));
+      })
+    );
+  }
+}
